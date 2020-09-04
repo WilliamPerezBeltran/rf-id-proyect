d5153d67173fbf4a5bc12477b033c988d635097b
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-09-03 20:04:23 -0500

    set-up sqlite and comunicate between main and redered process

--- public/electron.js
+++ public/electron.js
@@ -2,13 +2,14 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
+
 const path = require('path');
 const isDev = require('electron-is-dev');
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow = new BrowserWindow({width: 900, height: 680,webPreferences: {     nodeIntegration: true   } });
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   if (isDev) {
     // Open the DevTools.
@@ -32,47 +33,30 @@ app.on('activate', () => {
   }
 });
 
+const sqlite3 = require('sqlite3');
+var db = new sqlite3.Database(':memory:');
+db.serialize(function () {
+  db.run("CREATE TABLE Products (name, barcode, quantity)");
 
-// const { app, BrowserWindow } = require('electron')
+  db.run("INSERT INTO Products VALUES (?, ?, ?)", ['product001', 'xxxxx', 20]);
+  db.run("INSERT INTO Products VALUES (?, ?, ?)", ['product002', 'xxxxx', 40]);
+  db.run("INSERT INTO Products VALUES (?, ?, ?)", ['product003', 'xxxxx', 60]);
 
-// function createWindow () {
-//   // Create the browser window.
-//   const win = new BrowserWindow({
-//     width: 800,
-//     height: 600,
-//     webPreferences: {
-//       nodeIntegration: true
-//     }
-//   })
+  db.each("SELECT * FROM Products", function (err, row) {
+    console.log(row);
+  });
+});
+db.close();
 
-//   // and load the index.html of the app.
-//   win.loadFile('index.html')
+const { ipcMain } = require('electron') 
 
-//   // Open the DevTools.
-//   win.webContents.openDevTools()
-// }
 
-// // This method will be called when Electron has finished
-// // initialization and is ready to create browser windows.
-// // Some APIs can only be used after this event occurs.
-// app.whenReady().then(createWindow)
-
-// // Quit when all windows are closed, except on macOS. There, it's common
-// // for applications and their menu bar to stay active until the user quits
-// // explicitly with Cmd + Q.
-// app.on('window-all-closed', () => {
-//   if (process.platform !== 'darwin') {
-//     app.quit()
-//   }
-// })
-
-// app.on('activate', () => {
-//   // On macOS it's common to re-create a window in the app when the
-//   // dock icon is clicked and there are no other windows open.
-//   if (BrowserWindow.getAllWindows().length === 0) {
-//     createWindow()
-//   }
-// })
-
-// // In this file you can include the rest of your app's specific main process
-// // code. You can also put them in separate files and require them here.
+ipcMain.on('example', async(event, arg) => {
+  console.log(event) // prints "ping"
+  try {
+    event.sender.send('asynReply','Main said: Async message received')
+  } catch (error) {
+    console.log('ipcmain error')
+    console.log(error)
+  }
+})
